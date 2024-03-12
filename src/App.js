f15a8a498d638e8dc8ca749be1eb8a058973c55e
sunil <you@example.com>
2024-03-12 22:54:36 +0530

    adding a new component for table and passing an array and accessing the same in child component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import TodoRowItem from './components/ToDoRowItem';
+import TodoTable from './components/ToDoTable';
 
 function App() {
 
   const todos = [
-    {rowNumber:1 , rowDescription: 'Hello World', rowAssigned: 'Self'},
-    {rowNumber:2 , rowDescription: 'Heelo is good', rowAssigned: 'Self2'}
+    { rowNumber: 1, rowDescription: 'Hello World', rowAssigned: 'Self' },
+    { rowNumber: 2, rowDescription: 'Heelo is good', rowAssigned: 'Self2' }
   ]
 
 
@@ -16,19 +17,7 @@ function App() {
           Your Todo's
         </div>
         <div className='card-body'>
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th scope='col'>#</th>
-                <th scope='col'>Description</th>
-                <th scope='col'>Assigned</th>
-              </tr>
-            </thead>
-            <tbody>
-              <TodoRowItem rowNumber={todos[0].rowNumber} rowDescription={todos[0].rowDescription} rowAssigned={todos[0].rowAssigned}/>
-              <TodoRowItem rowNumber={todos[1].rowNumber} rowDescription={todos[1].rowDescription} rowAssigned={todos[1].rowAssigned}/>
-            </tbody>
-          </table>
+          <TodoTable todos={todos} />
         </div>
       </div>
     </div>
