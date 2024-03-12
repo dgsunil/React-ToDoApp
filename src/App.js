1e0f3b0cf82a5db47824666625e9e1beb4732e64
Sunil Dg <sunildg@in.ibm.com>
2024-03-12 22:24:38 +0530

    First commit, creating of table and row data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoRowItem from './components/ToDoRowItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mt-5 container'>
+      <div className="card">
+        <div className='card-header'>
+          Your Todo's
+        </div>
+        <div className='card-body'>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>Walk</td>
+                <td>Self</td>
+              </tr>
+              <tr>
+                <th scope='row'>2</th>
+                <td>Hair cut</td>
+                <td>Other</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
