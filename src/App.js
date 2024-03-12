6946b1b87cb5c4aabf04b0f96bd29757189937be
sunil <you@example.com>
2024-03-12 23:19:53 +0530

    onclick action and useState example, with useState the application still keeps the data and applies any changes made to the code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
+import React, {useState} from 'react';
 import './App.css';
-import TodoRowItem from './components/ToDoRowItem';
 import TodoTable from './components/ToDoTable';
 
 function App() {
 
-  const todos = [
+  const [todos, setToDos] = useState([
     { rowNumber: 1, rowDescription: 'Hello World', rowAssigned: 'Self' },
     { rowNumber: 2, rowDescription: 'Heelo is good', rowAssigned: 'Self2' },
     { rowNumber: 3, rowDescription: 'Hello ', rowAssigned: 'Self' },
     { rowNumber: 4, rowDescription: ' World', rowAssigned: 'Self' }
-  ]
+  ])
+
+  const addTodo = () => {
+    //console.log("Button is clicked");
+    //alert("Button is clicked");
+
+    if(todos.length >0){
+      const newTodo = {
+        rowNumber: todos.length+1,
+        rowDescription: "Data " + (todos.length+1),
+        rowAssigned: "Self " + (todos.length+1)
+      }
+
+      //todos.push(newTodo)
+      setToDos(todos => [...todos, newTodo])
+      // console.log(todos)
+    }
+  }
 
 
   return (
@@ -20,6 +37,7 @@ function App() {
         </div>
         <div className='card-body'>
           <TodoTable todos={todos} />
+          <button className='btn btn-primary' onClick={addTodo}>Add New ToDo</button>
         </div>
       </div>
     </div>
